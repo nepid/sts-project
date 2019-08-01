6150b3ee52641e42436aa4d24ab7f5609967f92b
dipen <dipen@DESKTOP-D26R8PQ>
2019-07-31 22:15:23 -0400

    uses of httpysrevlet requesst add in comment

--- FirtWebApp/src/main/java/com/dipen/demo/HomeController.java
+++ FirtWebApp/src/main/java/com/dipen/demo/HomeController.java
@@ -8,7 +8,17 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 	@RequestMapping("home")
+	
+//	public String homepage(HttpServletRequest req) {
+//		String name = req.getParameter("name");
+//		return home;
+//	}
+	
+	
 	public ModelAndView homepage(@RequestParam("name") String userName) {
+	
+		
+		System.out.println("home controller");
 		
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("name", userName);
